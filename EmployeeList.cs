df4e32a3c87d31f5f797306ffc6c66f1565791f3
Samy <frankinnunez@gmail.com>
2017-10-11 12:15:12 -0700

    changes to EmployeeList

--- EmployeeList.cs
+++ EmployeeList.cs
@@ -9,10 +9,22 @@ namespace AddicTech_Library
     class EmployeeList
     {
         private List<Employee> _employeeList;
+        private int _couner;
 
-        public EmployeeList(Employee employee){
-           
+        public EmployeeList(){
+        }
 
+        //Methods
+
+        //Add item
+        public void addEmployee(Employee employee){
+            if(employee != null){
+                _employeeList.Add(employee);
+                _couner++;
+            }else
+            {
+                Console.WriteLine("Enter an employee object");
+            }
         }
 
 
